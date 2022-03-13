442daa4ed9e632fb6502b9795076493f808234e3
Chris Chuang <67641657+cchuan3@users.noreply.github.com>
2022-03-13 16:38:26 -0500

    Cylinder Level - Rotation
    Simplified rotation implemented
    - L/R input causes level to rotate

---
+++ Assets/Scripts/LevelRotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelRotation : MonoBehaviour
+{
+    [SerializeField, Range(0f, 100f)]
+    float angularSpeed = 30f;
+
+
+    Rigidbody rb;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+	private void Update() {
+        Rotate();
+    }
+
+    private void Rotate() {
+        Vector3 desiredRotation = new Vector3(0, 0, -Input.GetAxis("Horizontal"));
+        Quaternion deltaRotation = Quaternion.Euler(desiredRotation * angularSpeed * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+    }
+}
---
+++ Assets/_Original Game Assets/Scripts/ChaserControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaserControl : MonoBehaviour
+{
+    [SerializeField, Range(0f, 100f)]
+    private float velocity = 5;
+
+    Rigidbody rb;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start() {
+        if (!GameManager.paused) {
+            CalculateSpeed();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Enemy") {
+            // Destroy obstacle
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void CalculateSpeed() {
+        velocity = (5 + GameManager.level) * (1 + GameManager.timer / 240);
+        rb.velocity = new Vector3(0, 0, velocity);
+    }
+
+    // Move chaser to just before a level
+    public void JumpToLevel(int level) {
+        Vector3 newPos = new Vector3(0, 5, (level - 1) * 210 - 20);
+        transform.position = newPos;
+        CalculateSpeed();
+    }
+
+    // Return how far the chaser is
+    public float GetProgress() {
+        return transform.position.z;
+    }
+}
