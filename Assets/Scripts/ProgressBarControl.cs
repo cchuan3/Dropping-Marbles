a1a9449352bc880a0d4deb33a3a82f243888908f
Chris Chuang <67641657+cchuan3@users.noreply.github.com>
2022-03-13 14:32:20 -0500

    Original Project
    The complete original project made for class

---
+++ Assets/Scripts/ProgressBarControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarControl : MonoBehaviour
+{
+    public Slider s;
+	private float currVal = 0f;
+    public float CurrVal {
+        get {
+            return currVal;
+        }
+        set {
+            currVal = value;
+            s.value = currVal;
+        }
+    }
+}
